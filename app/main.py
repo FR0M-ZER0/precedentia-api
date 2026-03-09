55ff2da02f8895a57557a17831e315e3ef8c97cd
João Suzuki <joaosuzuki98@outlook.com>
2026-03-09 15:27:13 -0300

    feat: Adiciona configuração do fastapi no main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.router import api_router
 
-app = FastAPI()
+app = FastAPI(
+    title="PrecedentIA API",
+    version="1.0.0"
+)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.include_router(api_router)
