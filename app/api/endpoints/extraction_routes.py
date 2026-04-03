63e708ce203391d2588077e7b7949e96c503a1f9
João Suzuki <joaosuzuki98@outlook.com>
2026-04-02 22:00:24 -0300

    feat(pia-60): Adiciona envio do resultado para o matching de precedentes

--- app/api/endpoints/extraction_routes.py
+++ app/api/endpoints/extraction_routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from app.services.extraction_service import ExtractionService
-from app.schemas.pdf_schema import PDFExtractionResponse
+from app.schemas.match_schema import MatchResponse
 
 router = APIRouter()
 
 
-@router.post("/extract", response_model=PDFExtractionResponse)
+@router.post("/extract", response_model=MatchResponse)
 async def extract_pdf_data(file: UploadFile = File(...)):
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Por Favor, envie um arquivo PDF.")
@@ -14,15 +14,13 @@ async def extract_pdf_data(file: UploadFile = File(...)):
         text = await file.read()
         result = await ExtractionService.extract_text_from_pdf(text)
 
-        external_response = await ExtractionService.send_petition_to_summary(
+        structured_petition = await ExtractionService.send_petition_to_summary(
             result["text"]
         )
 
-        return PDFExtractionResponse(
-            filename=file.filename,
-            petition=external_response,
-            total_pages=result["count"],
-        )
+        precedents = await ExtractionService.send_to_embedding(structured_petition)
+
+        return precedents
     except Exception as e:
         print(e)
         raise HTTPException(
