05c349099553222cb09384b247c0d51baf8f2a45
AvyaAquino <avyaaquino@gmail.com>
2026-05-20 10:31:27 -0300

    chore(pia-102): ajusta alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,10 @@ from alembic import context
 
 from app.core.database import Base
 
+from app.models.user_model import User
+from app.models.search_model import Search
+from app.models.petition_model import Petition
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
