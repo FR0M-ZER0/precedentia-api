e12aea78ec4b73e1164d722d63f13e6bc68b4fbd
João Suzuki <joaosuzuki98@outlook.com>
2026-05-21 14:11:55 -0300

    feat(pia-103): Adiciona a integração com o banco de dados

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -20,6 +20,6 @@ class User(Base):
     searches = relationship(
         "Search", back_populates="owner", cascade="all, delete-orphan"
     )
-    petitions = relationship(
-        "Petition", back_populates="user", cascade="all, delete-orphan"
-    )
+    # petitions = relationship(
+    #     "Petition", back_populates="user", cascade="all, delete-orphan"
+    # )
